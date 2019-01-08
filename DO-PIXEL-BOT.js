1fbcaf1e454fed2ca86db4f7177b170b922e5490
coding <slamjeron@gmail.com>
2019-01-07 17:10:08 -0700

    dASDF

--- DO-PIXEL-BOT.js
+++ DO-PIXEL-BOT.js
@@ -3,17 +3,10 @@
 // +-------------------------------------------+
 
 var CONFIG_USE_AUTO_LOGIN = true;
-var charnum=0
-var CONFIG_AUTO_LOGIN_USERNAME = "dark.shadow1";
-var CONFIG_AUTO_LOGIN_PASSWORD = "slam6550";
-if (charnum===0){
-	CONFIG_AUTO_LOGIN_USERNAME = "dark.shadow1";
-	CONFIG_AUTO_LOGIN_PASSWORD = "slam6550";
-}
-if (charnum===1){
-	CONFIG_AUTO_LOGIN_USERNAME = "dont*tread*on*me";
-	 CONFIG_AUTO_LOGIN_PASSWORD = "1234Iamcool";
-}
+var charnum=1
+var CONFIG_AUTO_LOGIN_USERNAME = "";
+var CONFIG_AUTO_LOGIN_PASSWORD = "";
+
 
 
 var CONFIG_AUTO_RECONNECT = true;
